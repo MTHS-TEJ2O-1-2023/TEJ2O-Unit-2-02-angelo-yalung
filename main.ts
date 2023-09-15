5c70707bcbbf8d2653f2a935f9886c569dbd2e7e
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-15 12:59:04 -0400

    typescript thingy ma jig

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Sep 2023
+ * This program writes "Hello, MTHS!" on the Microbit
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
